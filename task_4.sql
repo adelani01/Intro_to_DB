af774a321d4e38e483613fc8c155f58da84a7085
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-07-06 22:27:33 +0100

    describing tables in sql

--- task_4.sql
+++ task_4.sql
@@ -2,4 +2,8 @@
 USE alx_book_store;
 
 -- list all column and describe the table
-SHOW COLUMN FROM books;
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
+
