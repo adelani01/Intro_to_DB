829d79ea9a19a42036bf58827e923a0b27b3bbae
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-07-06 22:34:07 +0100

    inserting row into tables in sql

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- indicate which database to work with
+USE alx_book_store;
+
+-- inserting data into customer table
+INSERT INTO customer
+(customer_id, customer_name, email, address)
+VALUES (1, "Cole Baidoo","cbaidoo@sandtech.com", "123 Happiness Ave");
