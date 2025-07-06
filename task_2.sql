7e576c577ea5c306ae9c46c839c8f9daf9722495
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-07-06 22:04:14 +0100

    creating tablesin sql

--- task_2.sql
+++ task_2.sql
@@ -1,8 +1,19 @@
-CREATE TABLE IF NOT EXISTS books;
+CREATE TABLE IF NOT EXISTS books(
+    author_id (Primary Key),
+author_name VARCHAR(215)
+)
 
-CREATE TABLE  IF NOT EXISTS authors;
+CREATE TABLE  IF NOT EXISTS authors(
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+)
 
-CREATE TABLE IF NOT EXISTS customers;
+CREATE TABLE IF NOT EXISTS customers(
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
 
 CREATE TABLE IF NOT EXISTS orders;
 
