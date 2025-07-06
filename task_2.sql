058a0ad45dc1421cbbc2931b6eb50ee564f3c9ac
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-07-06 22:07:09 +0100

    creating tablesin sql

--- task_2.sql
+++ task_2.sql
@@ -1,20 +1,43 @@
-CREATE TABLE IF NOT EXISTS books(
-    author_id (Primary Key),
-author_name VARCHAR(215)
-)
+-- Stores information about authors.
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
 
-CREATE TABLE  IF NOT EXISTS Authors(
-    author_id PRIMARY KEY,
-    author_name VARCHAR(215)
-)
 
-CREATE TABLE IF NOT EXISTS customers(
-    customer_id PRIMARY KEY,
+-- tores information about books available in the bookstore.
+CREATE TABLE IF NOT EXISTS Books(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+   
+);
+
+-- Stores information about customers.
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id  INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
-)
+);
 
-CREATE TABLE IF NOT EXISTS orders;
+-- Stores information about orders placed by customers.
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
 
-CREATE TABLE IF NOT EXISTS order details;
+-- Stores information about the books included in each order.
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT,
+    order_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id), 
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
