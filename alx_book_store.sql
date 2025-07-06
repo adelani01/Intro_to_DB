366761b132adc94f41a4797c89979dd1b4c4ca73
emmanuel damilare adelani <adelanidamilare@gmail.com>
2025-07-06 21:34:41 +0100

    Add SQL schema

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -44,6 +44,6 @@ CREATE TABLE IF NOT EXISTS Order_Details(
     book_id INT,
     order_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id), 
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
